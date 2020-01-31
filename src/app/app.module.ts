0b862a3d34abce4e889b1946bd698c861c312303
Deddy Kosasih <deddy.kosasih@carsales.com.au>
2020-01-31 15:53:58 +1100

    bug replication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
